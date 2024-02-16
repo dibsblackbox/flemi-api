3f85542f159b418dbd6a1e796a91ded12e962864
dibsblackbox <150026954+dibsblackbox@users.noreply.github.com>
2024-02-16 17:01:41 +0530

    AI commit @ 2/16/2024, 5:01:15 PM

--- flemi/emails.py
+++ flemi/emails.py
@@ -1,14 +1,22 @@
-def send_email(recipients: list, subject=None, template=None, **kwargs):
-    pass  # TODO
-
-
 def send_email(recipients: List[str], subject: Optional[str] = None, template: Optional[str] = None, **kwargs) -> None:
+    """
+    Sends an email to the specified recipients, using the given subject and template.
+
+    Args:
+        recipients (List[str]): A list of email addresses to send the email to.
+        subject (Optional[str], optional): The subject line of the email. Defaults to None.
+        template (Optional[str], optional): The template to use for the email body. Defaults to None.
+        **kwargs: Additional keyword arguments to pass to the email sending function.
+
+    Returns:
+        None
+    """
     pass  # TODO
 
 
 def send_email(recipients: List[str], subject: Optional[str] = None, template: Optional[str] = None, **kwargs) -> None:
     """
-    Sends an email to the specified recipients using the given subject and template.
+    Sends an email to the specified recipients, using the given subject and template.
 
     Args:
         recipients (List[str]): A list of email addresses to send the email to.
